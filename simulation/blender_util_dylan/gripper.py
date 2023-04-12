83609a4d7231a34a2d6b7686363d7a0a30f69e48
Dylan Colli <dylanfrankcolli@gmail.com>
2023-04-12 15:08:54 -0400

    Change gripper animation to take frame duration
    - Takes in frame duration instead of the end frame now so we don't have
    to worry about passing in non-sequential frame numbers.

--- simulation/blender_util_dylan/gripper.py
+++ simulation/blender_util_dylan/gripper.py
@@ -28,15 +28,17 @@ def require_virtual_gripper(obj: bpy.types.Object):
 
 class GripperAnimation:
     """Class for easily defining piecewise linear gripper control with Bezier interpolation"""
-    obj: bpy.types.Object
+    gripper_obj: bpy.types.Object
+    grasped_obj: bpy.types.Object
     animation_frame_prev: int
     animation_loc_prev: mathutils.Vector
     blender_fps: float
     # Adding this as actions should have distinct names.
     __fcurve_group_num: int = 1
 
-    def __init__(self, obj: bpy.types.Object):
-        self.obj = obj
+    def __init__(self, gripper_obj: bpy.types.Object, grasped_obj: bpy.types.Object):
+        self.gripper_obj = gripper_obj
+        self.grasped_obj = grasped_obj
         self.blender_fps = bpy.context.scene.render.fps / bpy.context.scene.render.fps_base
 
         self.__fcurve_group = f"gripper_action_{GripperAnimation.__fcurve_group_num}"
@@ -48,7 +50,7 @@ class GripperAnimation:
 
         self.__add_init_keyframe_if_none_exist()
 
-    def add_movement(self, direction_vec: np.ndarray, velocity_mps: float, end_frame: int):
+    def add_movement(self, direction_vec: np.ndarray, velocity_mps: float, frame_duration: int):
         """Adds a movement to the gripper animation by adding a KeyFrame at the ending Frame
         
         TODO: This may get slow for gripper animations involving lots of keyframes as we update the
@@ -61,47 +63,45 @@ class GripperAnimation:
             Normalized vector indicating direction of linear movement.
         velocity_mps : float
             Velocity of motion in meters / second.
-        end_frame : int
-            The frame with which to insert the keyframe.
+        frame_duration : int
+            The number of frames this movement should take.
         """
-        # Assert that the given end_frame is after the last keyframe in the action sequence.
-        # I don't believe this is actually necessary but it is a code smell since I can't think of a 
-        # good reason to insert actions out of order.
-        assert (end_frame > self.animation_frame_prev), "Must insert keyframe at frame *after* the current last keyframe"
-
         # Find where the object will be at end_frame given the unit direction vector and velocity
-        frame_range = end_frame - self.animation_frame_prev
-        
-        # Convert the given velocity from [meters / second] to [meters / frame] and find final 
-        # location.
-        velocity_mpf = velocity_mps / self.blender_fps
-        loc_delta = mathutils.Vector(direction_vec * velocity_mpf * frame_range)
+        end_frame = frame_duration + self.animation_frame_prev
+        velocity_mpf = velocity_mps / self.blender_fps  # [meters / frame]
+        loc_delta = mathutils.Vector(direction_vec * velocity_mpf * frame_duration)
         loc_end = self.animation_loc_prev + loc_delta
 
         # Set the current frame to `end_frame` and move the object since keyframe_insert pulls 
         # location data from the object's current location.
         bpy.context.scene.frame_set(end_frame)
-        self.animation_loc_prev = self.obj.location = loc_end
+        self.animation_loc_prev = self.gripper_obj.location = loc_end
 
-        self.obj.keyframe_insert(data_path="location", group=self.__fcurve_group, frame=end_frame)
+        self.gripper_obj.keyframe_insert(data_path="location", group=self.__fcurve_group,
+                                         frame=end_frame)
 
         self.animation_frame_prev = end_frame
 
+        # Update the physics sim end frame so that we always simulate up to the last keyframe.
+        self.grasped_obj.modifiers['CLOTH'].point_cache.frame_end = end_frame
+
+
     def __add_init_keyframe_if_none_exist(self, frame_start=0):
         # if self.obj. # no keyframes present:
-        if self.obj.animation_data is None:
+        if self.gripper_obj.animation_data is None:
             # This means that there is no animation associated with the object so we need to add an 
             # initial keyframe at `frame_start` for the object's current location.
-            self.obj.keyframe_insert(data_path="location", frame=frame_start, group=self.__fcurve_group)
+            self.gripper_obj.keyframe_insert(data_path="location", frame=frame_start,
+                                             group=self.__fcurve_group)
             self.animation_frame_prev = frame_start
         else:
             # Grab the frame of the last keyframe. NOTE: For some reason this is a float but Blender
             # expects input to bpy.context.frame_set to be an int.
-            self.animation_frame_prev = int(self.obj.animation_data.action.frame_end)
+            self.animation_frame_prev = int(self.gripper_obj.animation_data.action.frame_end)
 
         # Set the active frame to the animation start frame so we can easily grab object location.
         print("Starting gripper keyframe frame:", self.animation_frame_prev)
         bpy.context.scene.frame_set(self.animation_frame_prev)
-        self.animation_loc_prev = self.obj.location
+        self.animation_loc_prev = self.gripper_obj.location
         
         
